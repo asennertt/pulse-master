867d522ac0235c30d971e7e10888af0a5c09473d
asennertt <asennertt@lotlyauto.com>
2026-02-24 01:00:18 -0500

    fix react router

--- Apps/Value/Pulse-Value-Files/apps/web/react-router.config.ts
+++ Apps/Value/Pulse-Value-Files/apps/web/react-router.config.ts
@@ -1,7 +1,8 @@
 import type { Config } from '@react-router/dev/config';
 
 export default {
-	appDirectory: './src/app',
-	ssr: true,
-	prerender: ['/*?'],
+  appDirectory: './src/app',
+  ssr: true,
+  // Change this line from ['/*?'] to false or remove it
+  prerender: false, 
 } satisfies Config;
